9dfc27afa3a3990fe041a4cb835cb20a0f4e1d82
Raj Kumar Ballepu <108982663+rajkumarballepu@users.noreply.github.com>
2024-07-27 13:02:55 +0530

    axiosFix1-rollupOption

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import {viteCommonjs} from "@originjs/vite-plugin-commonjs"
 import react from '@vitejs/plugin-react'
-import axios from 'axios'
 
 // https://vitejs.dev/config/
 export default defineConfig({
